cf323cc1899c3bac503dd7f460d79b68d24cf9a1
elements72 <antonio.lopez.8523@gmail.com>
2025-07-02 12:39:10 +0200

    Minor updates

--- setup.sh
+++ setup.sh
@@ -13,26 +13,25 @@ fi
 REPO_DIR="lm-evaluation-harness"
 METRICS_DIR="$(pwd)"
 
-# Clone the repo if it doesn't exist
-if [ ! -d "$REPO_DIR" ]; then
-    git clone --depth 1 https://github.com/EleutherAI/lm-evaluation-harness
-else
-    echo "$REPO_DIR already exists."
-fi
-
-cd "$REPO_DIR" || { echo "Failed to enter $REPO_DIR"; exit 1; }
-
 # Check if lm-evaluation-harness is already installed
 if pip show lm-eval > /dev/null 2>&1; then
     echo "lm-evaluation-harness appears to be installed."
     read -p "Do you want to reinstall it? [y/N]: " reinstall
     if [[ "$reinstall" =~ ^[Yy]$ ]]; then
+        cd "$REPO_DIR" || { echo "Failed to enter $REPO_DIR"; exit 1; }
         pip install -e .
         pip install -e .[vllm]
     else
         echo "Skipping reinstallation."
     fi
 else
+      # Clone the repo if it doesn't exist
+    if [ ! -d "$REPO_DIR" ]; then
+        git clone --depth 1 https://github.com/EleutherAI/lm-evaluation-harness
+    else
+        echo "$REPO_DIR already exists."
+    fi
+    cd "$REPO_DIR" || { echo "Failed to enter $REPO_DIR"; exit 1; }
     pip install -e .
     pip install -e .[vllm]
 fi
